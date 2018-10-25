22bed67ad05b359f8366c11577efbf29562082f4
Yisas The Manuel <yisasthemanuel@gmail.com>
2018-10-25 15:58:04 +0200

    Reorganización de imports en ManagerController

--- apirest/src/main/java/org/jlobato/gpro/rest/ManagerController.java
+++ apirest/src/main/java/org/jlobato/gpro/rest/ManagerController.java
@@ -1,19 +1,13 @@
 package org.jlobato.gpro.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.jlobato.gpro.services.IManagerService;
 import org.jlobato.gpro.services.ManagerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/managers")
